94f446ba15555ef5a220f77d31400b6c6818e44e
Henrique <henrique1jose@gmail.com>
2024-09-04 11:19:31 -0300

    Foi feito a criação dos objetos

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        id: 1,
+        titulo: "Arrascaeta",
+        descricao: "Giorgian De Arrascaeta, conhecido simplesmente como Arrascaeta, é um futebolista uruguaio que atua como meia. Atualmente, defende o Flamengo. Conhecido por sua habilidade técnica e visão de jogo, é considerado um dos principais jogadores do futebol sul-americano",
+        link: "https://pt.wikipedia.org/wiki/Giorgian_de_Arrascaeta",
+        esporte: "Futebol"
+    },
+    {
+        id: 2,
+        titulo: "Ronaldinho Gaúcho",
+        descricao: "Ronaldinho Gaúcho, nome artístico de Ronaldo de Assis Moreira, é um ex-futebolista brasileiro que atuava como meia-atacante. Considerado um dos maiores jogadores de todos os tempos, é conhecido por sua habilidade técnica, dribles e gols espetaculares.",
+        link: "https://pt.wikipedia.org/wiki.org/wiki/Ronaldinho_Gaúcho",
+        esporte: "Futebol"
+    },
+    {
+        id: 3,
+        titulo: "Neymar Jr.",
+        descricao: "Neymar da Silva Santos Júnior, mais conhecido como Neymar Jr., é um dos maiores craques do futebol brasileiro da atualidade. Com um talento inegável e uma habilidade única para driblar adversários, Neymar conquistou o mundo com seus dribles desconcertantes e gols espetaculares.",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        esporte: "Futebol"
+    }
+];
