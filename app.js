9ea441a6fd294a682645e5b204e63f5288fe7442
Henrique <henrique1jose@gmail.com>
2024-09-05 11:12:17 -0300

    Apresentando funcionalidades ao buscar, e redefinindo algumas variáveis

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log(dados);
+function pesquisar() {
+    console.log(dados);
+
+    let section = document.getElementById('resultados-pesquisa');
+    console.log(section);
+
+    let resultados = ""; // Inicializa a string vazia
+
+    // Adiciona os dados dentro da string 'resultados' e não diretamente no 'innerHTML'
+    for (let dado of dados) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Para mais informações</a>
+        </div>
+        `;
+    }
+
+    // Após o loop, insira a string 'resultados' no 'innerHTML' da section
+    section.innerHTML = resultados;
+}
+
